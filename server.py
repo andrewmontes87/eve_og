730d578f4a375219bed4bc587801a7c198525b54
andrewmontes87 <andrewmontes87@gmail.com>
2016-09-01 21:33:57 -0700

    please clap

--- server.py
+++ server.py
@@ -1,14 +1,4 @@
 from eve import Eve
-from settings import URL_PREFIX, X_DOMAINS, HATEOAS, PAGINATION, MONGO_URI, DOMAIN 
-
-SETTINGS = {
-    'URL_PREFIX': URL_PREFIX,
-    'X_DOMAINS': X_DOMAINS,
-    'HATEOAS': HATEOAS,
-    'PAGINATION': PAGINATION,
-    'MONGO_URI': MONGO_URI,
-    'DOMAIN': DOMAIN
-}
 
 app = Eve()
 
--- settings.py
+++ settings.py
@@ -6,11 +6,17 @@ HATEOAS = False
 PAGINATION = False
 
 MONGO_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/og_co')
-# MONGO_DBNAME = 'og_co'
+
 DOMAIN = { 
     'og_co':{
         'item_title':'og_co_record',
         'schema':{
+            "_id": {
+                'type': 'dict',
+                'schema': {
+                    '$oid':{'type':'string'}
+                }
+            },
             'link':{'type':'string'},
             'country':{'type':'string'},
             'headquarters_location':{'type':'string'},
@@ -18,7 +24,7 @@ DOMAIN = {
             'coordinate_location':{'type':'string'},
             'bio_img':{'type':'string'},
             'image_urls':{'type':'array'},
-            'revenue':{'type':'string'},            
+            'revenue':{'type':'float'},            
             'inception':{'type':'string'},            
             'instance_of':{'type':'string'},            
             'isin':{'type':'string'},            
@@ -26,3 +32,20 @@ DOMAIN = {
         'url':'og_co'
     }
 }
+# {
+#     "_id": {
+#         "$oid": "57c8fc48e0831eea5bcb8e0e"
+#     },
+#     "bio_img": "full/811ee4352837f20673bd0dd3c937fe603a7d1769.jpg",
+#     "name": "Royal Dutch Shell",
+#     "revenue": 265,
+#     "country": "United Kingdom",
+#     "image_urls": [
+#         "https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/Shell_logo.svg/120px-Shell_logo.svg.png"
+#     ],
+#     "isin": "GB00B03MLX29",
+#     "link": "http://en.wikipedia.org/wiki/Royal_Dutch_Shell",
+#     "coordinate_location": "51.5072222222, -0.1275",
+#     "inception": "2005",
+#     "instance_of": "business enterprise"
+# }
