746be77b22b50d21d898195d7865c1ae4c2a9ccf
andrewmontes87 <andrewmontes87@gmail.com>
2016-09-01 21:28:26 -0700

    please clap

--- server.py
+++ server.py
@@ -1,7 +1,16 @@
 from eve import Eve
+from settings import URL_PREFIX, X_DOMAINS, HATEOAS, PAGINATION, MONGO_URI, DOMAIN 
+
+SETTINGS = {
+    'URL_PREFIX': URL_PREFIX,
+    'X_DOMAINS': X_DOMAINS,
+    'HATEOAS': HATEOAS,
+    'PAGINATION': PAGINATION,
+    'MONGO_URI': MONGO_URI,
+    'DOMAIN': DOMAIN
+}
 
 app = Eve()
 
-
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(settings=SETTINGS, debug=True)
