37365d2756e4490136a770d2ec4b35f90085dd71
andrewmontes87 <andrewmontes87@gmail.com>
2016-09-01 21:44:12 -0700

    maybe?

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ X_DOMAINS = '*'
 HATEOAS = False
 PAGINATION = False
 
-MONGO_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/og_co')
+MONGO_URI = os.environ.get('MONGODB_URI'+'/og_co', 'mongodb://localhost:27017/og_co')
 
 DOMAIN = { 
     'og_co':{
@@ -26,20 +26,3 @@ DOMAIN = {
         'url':'og_co'
     }
 }
-# {
-#     "_id": {
-#         "$oid": "57c8fc48e0831eea5bcb8e0e"
-#     },
-#     "bio_img": "full/811ee4352837f20673bd0dd3c937fe603a7d1769.jpg",
-#     "name": "Royal Dutch Shell",
-#     "revenue": 265,
-#     "country": "United Kingdom",
-#     "image_urls": [
-#         "https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/Shell_logo.svg/120px-Shell_logo.svg.png"
-#     ],
-#     "isin": "GB00B03MLX29",
-#     "link": "http://en.wikipedia.org/wiki/Royal_Dutch_Shell",
-#     "coordinate_location": "51.5072222222, -0.1275",
-#     "inception": "2005",
-#     "instance_of": "business enterprise"
-# }
